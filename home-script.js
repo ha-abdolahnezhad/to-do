95ed2d08932990adf571256584c456ef3148aaa1
Hamed Abdolahnezhad <ab.hamed.it@gmail.com>
2022-08-16 00:51:42 +0430

    modified home and task list

--- home-script.js
+++ home-script.js
@@ -1,68 +1,55 @@
 //----- home ----
+//get data from storage
 const tasks = JSON.parse(localStorage.getItem('task') || '[]');
 // console.log(tasks);
+// select html elements
 const totalTasks = document.querySelector(".total");
 const leftTasks = document.querySelector(".left");
 const workTasks = document.querySelector(".work-tasks");
 const personalTasks = document.querySelector(".personal-tasks");
 const shoppingTasks = document.querySelector(".shopping-tasks");
 const healthTasks = document.querySelector(".health-tasks");
+const otherTasks = document.querySelector(".other-tasks");
 const todayTasks = document.querySelector(".today");
-
-let today = new Date();
+//get date and time
+let today = new Date().toISOString().slice(0, 10);
 //console.log(today);
-const dd = String(today.getDate()).padStart(2, '0');
-const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-const yyyy = today.getFullYear();
 
-today = yyyy + '-' + mm + '-' + dd;
-// console.log(today);
 
-const tasksCount = 
-{
-    work : 0,
-    personal : 0,
-    shopping : 0,
-    health : 0,
-    other : 0,
-    dayCount: 0,
-}
-
+//times
 const times = ["08:00", "08:30", "09:00", "09:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00", "18:30", "19:00", "19:30"];
 
-tasks.filter(function(task) {
-    if(task.type === "work"){
-        tasksCount.work++;
-    }
-});
-tasks.filter(function(task) {
-    if(task.type === "personal"){
-        tasksCount.personal++;
-    }
-});
-tasks.filter(function(task) {
-    if(task.type === "shopping"){
-        tasksCount.shopping++;
-    }
-});
-tasks.filter(function(task) {
-    if(task.type === "health"){
-        tasksCount.health++;
-    }
-});
-tasks.filter(function(task) {
-    if(task.date === today){
-        tasksCount.dayCount++;
-    }
-});
+let tasksCount = {
+    work: 0,
+    personal: 0,
+    shopping: 0,
+    health: 0,
+    other: 0,
+    dayCount: 0,
+    leftTasks: 0
+};
+const count = tasks.reduce(function(obj , item) {
+    obj[item.type]++;
+    if(item.date === today) obj.dayCount++;
+    if(new Date(item.date) < new Date(today)) obj.leftTasks++;
+    return obj;
+}, tasksCount);
 
+
+console.log(count);
+tasks.filter(function(task) {
+    
+})
+//show tasks
 totalTasks.innerText = tasks.length;
+leftTasks.innerText = tasks.leftTasks;
 workTasks.innerText = `${tasksCount.work} tasks`;
 personalTasks.innerText = `${tasksCount.personal} tasks`;
 shoppingTasks.innerText = `${tasksCount.shopping} tasks`;
 healthTasks.innerText = `${tasksCount.health} tasks`;
+otherTasks.innerText = `${tasksCount.other} tasks`;
 todayTasks.innerText = `Today you have ${tasksCount.dayCount} tasks`;
-
+//append card to time list
 const timeBox = document.querySelector(".list");
 for(let i = 0; i < 24; i++) {
     const li = document.createElement("li");
--- task-list-script.js
+++ task-list-script.js
@@ -1,4 +1,5 @@
 //----task list----
+//search bar
 const btnBack = document.querySelector(".btn-back");
 const btnClose = document.querySelector(".btn-close");
 const btnSearch = document.querySelector(".btn-search");
@@ -19,3 +20,50 @@ btnClose.addEventListener("click", function() {
     searchInput.style.display = "none";
     btnClose.style.display = "none";
 });
+// load data
+const tasks = JSON.parse(localStorage.getItem('task') || '[]');
+const list = document.querySelector(".list");
+tasks.forEach(element => {
+    const li = document.createElement("li");
+    li.classList.add('list-item');
+    li.innerHTML = `<div class="edit-block">
+                        <a href="task-list.html">
+                            <img class="edit" src="./assets/edit.png" alt="">
+                        </a>
+                    </div>
+                    <div class="content-block">
+                        <span class="bold">${element.title}</span>
+                        <span>${element.description}</span>
+                    </div>
+                    <div class="circle ${element.type}"></div>
+                    <div class="delete-block">
+                        <img class="trash" src="./assets/trash.png" alt="">
+                    </div>`;
+    list.appendChild(li);
+});
+
+//hover list items
+listItem = document.querySelectorAll(".list-item");
+editElement = document.querySelectorAll(".edit-block");
+circleElement = document.querySelectorAll(".circle");
+trashIcon = document.querySelectorAll(".trash");
+listItem.forEach(function(element, index) {
+    trashIcon[index].style.display = "none";
+    element.addEventListener("mouseenter", function() {
+        editElement[index].style.display = "none";
+        circleElement[index].style.display = "none";
+        trashIcon[index].style.display = "initial";
+        element.classList.remove("list-item");
+        element.classList.add("item-selected");
+    });
+    element.addEventListener("mouseleave", function() {
+        editElement[index].style.display = "initial";
+        circleElement[index].style.display = "initial";
+        trashIcon[index].style.display = "none";
+        element.classList.remove("item-selected");
+        element.classList.add("list-item");
+    });
+});
+
+
+
